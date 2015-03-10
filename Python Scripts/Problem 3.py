b8756550ec0def3d2470da71f70a0f5834c1a5db
javsav <javed.alam@student.adelaide.edu.au>
2015-03-10 15:37:10 +1030

    3.py
    more spring cleaning

--- Python Scripts/Problem 3.py
+++ Python Scripts/Problem 3.py
@@ -1,6 +1,4 @@
-### Problem 3
-
-#### <i>The prime factors of 13195 are 5, 7, 13 and 29.  What is the largest prime factor of the number 600851475143 ?</i> 
+#The prime factors of 13195 are 5, 7, 13 and 29.  What is the largest prime factor of the number 600851475143 ?
 
 
 def isprime(n):
@@ -17,15 +15,11 @@ def isprime(n):
             return False
     return True
 	
+p = 600851475143
+max = 0
+for i in xrange (1, int(p**0.5)):
+    if p % i == 0 and isprime(i)==True:
+		max = i
+print str(max) + " is the largest prime factor of the number 600851475143 "
 
-array = []
-i=1
-while i<775147:
-    if 600851475143 % i == 0 and isprime(i)==True:
-        array.append(i)
       
-    i += 1
-print array
-
-
-##### Answer: 6857
