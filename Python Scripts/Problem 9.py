bae1ca366bf8056ca7302a5ca3059555bb7afce7
javsav <javed.alam@student.adelaide.edu.au>
2015-03-10 14:44:20 +1030

    Updated 9.py
    Used same algorithm as for ruby and C++ instead of brute force

--- Python Scripts/Problem 9.py
+++ Python Scripts/Problem 9.py
@@ -1,20 +1,12 @@
-### Problem 9
-
-#### <i>A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,  a^2 + b^2 = c^2  For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.  There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc. </i>
-
-
-
-for a in range (1,500):
-    for b in range (1, 500):
-        for c in range (1, 500):
-            if a+b+c == 1000 and c**2==a**2+b**2 and a<b and b<c:
-                d=a*b*c
-                print a
-                print b
-                print c
-                print "answer: "
-                print d
-                break
-
-
-#####  Answer:  31875000  Completed on Mon, 9 Jun 2014, 03:32 
+max = 1000 # a + b + c = 1000
+for a in range (1,max/3 +1): # Essentially a for loop in range 1 to 333.3 (step = 1). Since a < b < c and a + b + c = 1000, possible max(a) = 1000/3
+    for b in range (a, max/2 +1): # For loop in range a to 500 (step = 1). Because a < b, a can be used as the start range of this for loop to decrease execution time. As above, max(b) = 1000/2
+        c = max - a - b # Because a + b + c = 1000, c = 1000 - a - b
+        if c**2 == a**2 + b**2: # If statement determines when the values of a^2 + b^2 == c^2 when c is equal to max - a - b
+            d=a*b*c # Euler solution requires the multiples of a b and c
+            print a
+            print b
+            print c # Printing values of a, b and c
+            print "answer: "
+            print d # Printing Euler answer
+               
