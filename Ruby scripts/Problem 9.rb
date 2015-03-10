65dadc74bf2ee46131490283a00a09a7bf0aca06
javsav <javed.alam@student.adelaide.edu.au>
2015-03-10 14:37:47 +1030

    Updated 9.rb with faster algorithm
    The one I used in my C++ solution

--- Ruby scripts/Problem 9.rb
+++ Ruby scripts/Problem 9.rb
@@ -1,9 +1,9 @@
-for a in 1..500
-	for b in 1..500
-		for c in 1..500
-			if a + b + c == 1000 and c**2 == a**2 + b**2 and a<b
-			puts a*b*c
-			end
+max = 1000 # a + b + c = 1000
+for a in 1..max/3 # Essentially a for loop in range 1 to 333.3 (step = 1). Since a < b < c and a + b + c = 1000, possible max(a) = 1000/3
+	for b in a..max/2 # For loop in range a to 500 (step = 1). Because a < b, a can be used as the start range of this for loop to decrease execution time. As above, max(b) = 1000/2
+	c = max - a - b # Because a + b + c = 1000, c = 1000 - a - b
+		if c**2 == a**2 + b**2 # If statement determines when the values of a^2 + b^2 == c^2 when c is equal to max - a - b
+			puts a*b*c # Printing the multiples of a*b*c for the Euler solution
 		end
 	end
 end
