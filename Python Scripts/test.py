2ee462800b225fb021a8e8d6a348f0d0eb908c5c
javsav <javed.alam@student.adelaide.edu.au>
2015-03-11 13:02:26 +1030

    deleting test file

--- Python Scripts/test.py
+++
@@ -1,7 +0,0 @@
-import itertools
-
-for j in itertools.count(1):
-	if j % 1 == 0 and j % 2 == 0 and j % 3 == 0 and j % 4 == 0 and j % 5 == 0 and j % 6 == 0 and j % 7 == 0 and j % 8 == 0 and j % 9 == 0 and j % 10 == 0 and j % 11 == 0 and j % 12 == 0 and j % 13 == 0 and j % 14 == 0 and j % 15 == 0 and j % 16 == 0 and j % 17 == 0 and j % 18 == 0 and j % 19 == 0 and j % 20 == 0:
-		break
-	
-print j
