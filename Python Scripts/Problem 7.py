643e3552f23b70447cf668a7747568f70e06592e
javsav <javed.alam@student.adelaide.edu.au>
2015-03-16 12:09:27 +1030

    7 updated
    ^

--- Python Scripts/Problem 7.py
+++ Python Scripts/Problem 7.py
@@ -1,27 +1,25 @@
-### Problem 7
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.  What is the 10 001st prime number?
 
-#### <i>By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.  What is the 10 001st prime number? </i>
-
-
+import itertools
 def isprime(n):
     
     n = abs(int(n))    
     if n < 2:
-        return False    
+        return 0    
     if n == 2: 
-        return True   
+        return 1   
     if n % 2 == 0: 
-        return False    
+        return 0    
     for x in range(3, int(n**0.5)+1, 2):
         if n % x == 0:
-            return False
-    return True
-pr = []
-for x in range (1,800000):
-    if isprime(x)==True:
-        pr.append(x)
+            return 0
+    return 1
 
-print pr[10000]
-
-
-##### Answer:  104743 
+length = []
+for x in itertools.count():
+	if isprime(x) == 1:
+		length.append(x)
+	if len(length) == 10001:
+		print str(length.pop(-1)) + " is the 10,001st prime number"
+		break
+		
